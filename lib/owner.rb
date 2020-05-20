5d24bf0273dc31d16795771df1ea8fc543e025d8
michael thomas <michael.thomas2014@gmail.com>
2020-05-20 13:35:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
+  def species
+    @species = "human"
+  end
+  
+  def say_spicies
+    puts species
+  end
+  
+  def self.all 
+    @all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
 end

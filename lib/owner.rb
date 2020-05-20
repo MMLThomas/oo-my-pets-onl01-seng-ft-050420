70a0738fc03c0e2e30c7a3a7606c560abb3c4fe0
michael thomas <michael.thomas2014@gmail.com>
2020-05-20 15:37:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,11 +55,11 @@ class Owner
   end
   
   def walk_dogs
-    dogs.each{|dog| dog.mood = "happy"}
+    @dogs.each{|dog| dog.mood = "happy"}
   end
   
   def feed_cats
-    cats.each{|cat| cat.mood = "happy"}
+    @cats.each{|cat| cat.mood = "happy"}
     
   end
   

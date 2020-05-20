b6709526b4f6b0b38a4a2ef30a36b03b820883dd
michael thomas <michael.thomas2014@gmail.com>
2020-05-20 15:39:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,14 +64,14 @@ class Owner
   end
   
   def sell_pets
-    cats.each{|cat| cat.mood = "nervous"}
-    dogs.each{|dog| dog.mood = "nervous"}
-    cats.each{|cat| cat.owner = nil}
-    dogs.each{|dog| dog.owner = nil}
+    @cats.each{|cat| cat.mood = "nervous"}
+    @dogs.each{|dog| dog.mood = "nervous"}
+    @cats.each{|cat| cat.owner = nil}
+    @dogs.each{|dog| dog.owner = nil}
   end
   
   def list_pets
-    "I have #{dogs.count} dogs and #{cats.count} cats."
+    "I have #{@dogs.count} dogs and #{@cats.count} cats."
   end
   
   

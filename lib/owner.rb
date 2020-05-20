aaafc79594798864c630bf800b26ae57d3ae7741
michael thomas <michael.thomas2014@gmail.com>
2020-05-20 14:10:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,15 +57,16 @@ class Owner
   end
   
   def walk_dogs
-    dogs.mode =
+    dogs.each{|dog| dog.mood = "happy"
   end
   
   def feed_cats
+    cats.each{|cat| cat.mood = "happy"
     
   end
   
   def sell_pets
-    
+    cats.each{|cat| cat.mood = "happy"
     
   end
   
